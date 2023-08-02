17744bb249d8145d0c30dd95abc2b6eca98261a5
Pawan Kumar <pawan4super30@gmail.com>
2023-08-02 23:45:15 +0530

    gh-pages installed for hosting the app in github Pagese............

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
     <Box width={'400px'} sx={{width: { xl: '1488px'}}} m='auto'>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="/fitness" element={<Home />} />
+        <Route path="/fitness/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
       <Footer />
     </Box>
